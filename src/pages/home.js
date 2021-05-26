2a85ad4b469f42062fe4894a9d15aad85f8a737e
Matthew Savin <matt.asavin@gmail.com>
2021-05-26 14:57:21 +0100

    fixed godawful formatting rules

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,24 +5,36 @@ function home() {
     return (
         <div className="home">
             <div className="entry">
-                <h1>Transformational Agility for IT and Business Strategies</h1>
-                <p>Driving Business Flexibility, Growth mindset, Enabling Change across IT teams and integrating them
-                    back to the business
-                    <br />
-                    From implementing new business strategies to ultra-efficient work processes,
-                    Transformational Business agility is ready to tackle any challenge and put you on the path to success.
-                    Since 2010, we’ve helped our clients by delivering IT capabilities that transform the way they do
-                    business. Contact us today to schedule a consultation.</p>
+                <div className="header">
+                    <h1>Transformational Agility for IT and Business Strategies</h1>
+                </div>
+                <div className="introduction">
+                    <p>
+                        Driving Business Flexibility, Growth mindset, Enabling Change across IT teams and integrating them
+                        back to the business
+                        <br />
+                        From implementing new business strategies to ultra-efficient work processes,
+                        Transformational Business agility is ready to tackle any challenge and put you on the path to success.
+                        Since 2010, we’ve helped our clients by delivering IT capabilities that transform the way they do
+                        business. Contact us today to schedule a consultation.
+                    </p>
+                </div>
+
             </div>
-            <div className="second left">
+            <div className="boxes">
+                <div className="second left">
+                </div>
+                <div className="third right">
+                    <p>this is text</p>
+                </div>
+                <div className="fourth left">
+                    <p>this is text</p>
+                </div>
+                <div className="fifth right">
+                </div>
             </div>
-            <div className="third right">
-                <p>this is text</p>
-            </div>
-            <div className="fourth left">
-                <p>this is text</p>
-            </div>
-            <div className="fifth right">
+            <div className="footer">
+                <p>&copy; Copyright 2021 Transformational Agility Ltd.</p>
             </div>
         </div>
     )
