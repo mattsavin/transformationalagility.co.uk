64de643bb84ec161ee8c796f17bd791a83eb6be3
Matthew Savin <matt.asavin@gmail.com>
2021-06-02 17:41:52 +0100

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,6 @@ import React from "react";
 import {Link} from "react-router-dom";
 import "./contact.css";
 import * as FAIcons from "react-icons/fa";
-import * as IOIcons from "react-icons/io";
 import * as RIIcons from "react-icons/ri";
 import smoothscroll from "smoothscroll-polyfill";
 
