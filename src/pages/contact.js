faeb27ff2eba0acd831b5ea40bee6ba47968a426
lamitron <matt.asavin@gmail.com>
2021-06-02 16:38:11 +0100

    update packages, added emailJS for future use. New branch dev created, for future development tests

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,13 @@ import React from "react";
 import {Link} from "react-router-dom";
 import "./contact.css";
 import * as FAIcons from "react-icons/fa";
-import * as IOIcons from "react-icons/io";
 import * as RIIcons from "react-icons/ri";
 import smoothscroll from "smoothscroll-polyfill";
+import { init } from 'emailjs-com';
 
+
+init("user_c02JIaEj90UAVhXU8Op5n");
 smoothscroll.polyfill();
-
 window.__forceSmoothScrollPolyfill__ = true;
 
 function contact() {
@@ -38,12 +39,8 @@ function contact() {
                             <a href="mailto:00savin@gmail.com">Mike Savin</a>
                         </p>
                     </div>
-                    <div className="contact-entry">
-                        <FAIcons.FaPhoneAlt className="contact-icon"/>
-                        <p>
-                            <a href="tel:+447990658292">+44 7990 658 292</a>
-                        </p>
-                    </div>
+                </div>
+                <div className="contact contact-form">
 
                 </div>
             </div>
